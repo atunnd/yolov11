7e4498a0b76556850f2058d72b688c15e08691b2
atunnd <atuan.ngndai@gmail.com>
2025-03-28 23:37:34 +0700

    adding python file run

---
+++ model_run.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+def run():
+    model = YOLO("yolo11n.pt")
+    train_results = model.train(
+        data = "../dataset/data.yaml",
+        epochs=1000,
+        batch=128,
+        device=0,
+        plots=True
+    )
+
+if __name__ == "__main__":
+    run()
