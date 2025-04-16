560acb92c0493a72f75836cd2c29fd3f89249c32
atunnd <atuan.ngndai@gmail.com>
2025-04-16 13:27:25 +0700

    adding training code for yolov12

---
+++ train_yolo12.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+
+model = YOLO("yolo12n.pt")
+
+model.train(
+    data="../vehicle_detection/data.yaml",  
+    epochs=1000,  
+    imgsz=640, 
+    batch=128, 
+    device=1,  
+    name="yolov12",
+    optimizer="AdamW",
+    cos_lr=True,
+    lr0=1e-3,
+    momentum=0.937,
+    weight_decay=0.0005,
+    warmup_epochs=3.0,
+    val=True,
+    plots=True,
+)
+
